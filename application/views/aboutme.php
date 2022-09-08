33411095585d9335224bdfd07b35e1ef2f4cb3bf
althafariq <althafariq04@gmail.com>
2022-09-08 20:30:15 +0700

    initial commit

---
+++ application/views/aboutme.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Me</title>
+</head>
+<body>
+        <h1>Tugas 1 Pemrograman Framework</h1>
+        <ul>
+            <li>Nama : Althaf Ariq Musyaffah Ghufron</li>
+            <li>NPM : 19081010102</li>
+            <li>Jurusan : Teknik Informatika</li>
+            <li>Fakultas : Ilmu Komputer</li>
+            <li>Email : 19081010102@student.upnjatim.ac.id</li>
+            <li>Alamat : jl Medokan Asri Timur IV RL5 E14, Kali Rungkut, Rungkut, Kota Surabaya, Jawa Timur</li>
+            <li>About me : Saat ini saya sedang menempuh kuliah Teknik Informatika semester 7 di Universitas Pembangunan Nasional Veteran Jawa Timur. Programming atau ngoding adalah jalan ninjaku</li>
+        </ul>
+</body>
+</html>
